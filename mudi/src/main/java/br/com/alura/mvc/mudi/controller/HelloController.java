1e7e5af297de30bda0d9190fed95c4d793714866
italo-baciliere <italoluciano57@gmail.com>
2023-01-10 15:27:25 -0300

    Criação do Produto | Inserção do Thymeleaf | Inserção do Bootstrap

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,17 +1,36 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("nome", "Dev");
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("MacBook Air M1");
+		pedido.setValorNegociado(new BigDecimal("6599"));
+		pedido.setDataEntrega(LocalDate.now());
+		pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_801112-MLA46516512347_062021-O.webp");
+		pedido.setUrlProduto("https://www.mercadolivre.com.br/apple-macbook-air-13-polegadas-2020-chip-m1-256-gb-de-ssd-8-gb-de-ram-ouro/p/MLB17828522?product_trigger_id=MLB17828523&quantity=1");		
+		pedido.setDescricao("13 polegadas, 2020, Chip M1, 256 GB de SSD, 8 GB de RAM");
+		
+		List<Pedido> listaDePedidos = Arrays.asList(pedido);
+		
+		model.addAttribute("pedidos", listaDePedidos);
+		
 		return "hello";
 	}
+	
 	
 }
 	
